b7c16695ea9d0fba13367fac303aa38f74f9c743
Valentina Zapotoczny <valen.czny@gmail.com>
2023-03-01 18:10:17 -0300

    agregué el menu de mobile al js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+//variables del document
 const menu_email = document.querySelector(".navbar-email");
 const desktop_menu = document.querySelector(".desktop-menu");
+const Burguer_menu = document.querySelector(".menu");
+const mobile_menu = document.querySelector(".mobile-menu");
+
 menu_email.addEventListener('click', toggleDesktopMenu);
+Burguer_menu.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
-    console.log("se hizo click");
     desktop_menu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+    mobile_menu.classList.toggle('inactive');
 }
