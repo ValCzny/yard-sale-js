a2ba794912aec552f655d45ad202742fb8c6ce26
Valentina Zapotoczny <valen.czny@gmail.com>
2023-03-20 13:10:41 -0300

    cambios al carrito de compras en la clase 21

--- main.js
+++ main.js
@@ -3,13 +3,37 @@ const menu_email = document.querySelector(".navbar-email");
 const desktop_menu = document.querySelector(".desktop-menu");
 const Burguer_menu = document.querySelector(".menu");
 const mobile_menu = document.querySelector(".mobile-menu");
+const menu_carrito_icon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menu_email.addEventListener('click', toggleDesktopMenu);
-Burguer_menu.addEventListener('click', toggleMobileMenu)
+Burguer_menu.addEventListener('click', toggleMobileMenu);
+menu_carrito_icon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+    const isAsideClosed =aside.classList.contains('inactve');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     desktop_menu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
+    const isAsideClosed =aside.classList.contains('inactve');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
     mobile_menu.classList.toggle('inactive');
+}
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobile_menu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobile_menu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive')
 }
