9f83b0e0b1d5fd5c2fbaf3bc438e1adfe2a69a38
Valentina Zapotoczny <valen.czny@gmail.com>
2023-03-21 19:34:35 -0300

    xd

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ productList.push({
 });
 productList.push({
     name: 'Laptop',
-    price: 320,
+    price: 420,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
 
